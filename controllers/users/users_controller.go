4e7e6ed089de91281363237bceb4d4cf39f384f6
Tomas Fregonese <tomas.fregonese@mercadolibre.com>
2022-02-11 09:02:41 -0300

    added create user service

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,9 +1,55 @@
 package users
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func GetUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/tfregonese/bookstore_users-api/domain/users"
+	"github.com/tfregonese/bookstore_users-api/services"
+	"github.com/tfregonese/bookstore_users-api/utils/errors"
+)
 
-func CreateUser(c *gin.Context) {}
+func GetUser(c *gin.Context) {
+	user, err := services.GetUser(1)
+	if err != nil {
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
 
-func SearchUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+	var user users.User
+	/*
+			bytes, err := ioutil.ReadAll(c.Request.Body)
+			if err != nil {
+				// Handle the Error
+				c.String(http.StatusBadRequest, err.Error())
+				return
+			}
+			if err := json.Unmarshal(bytes, &user); err != nil {
+				// If there is no error in the body
+				c.String(http.StatusBadRequest, err.Error())
+				return
+			}
+		This can be replaced for:
+	*/
+	if err := c.ShouldBindJSON(&user); err != nil {
+		//Handle error
+		restErr := errors.NewBadRequestError("Invalid Json")
+		c.JSON(http.StatusBadRequest, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		//Handle saveErr
+		c.JSON(http.StatusBadRequest, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
+}
+
+func SearchUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me!")
+}
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,18 @@
 package services
+
+import (
+	"github.com/tfregonese/bookstore_users-api/domain/users"
+	"github.com/tfregonese/bookstore_users-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+}
+
+func GetUser(user_id int64) (*users.User, *errors.RestErr) {
+	return &users.User{
+		Id:        123,
+		FirstName: "Pepe",
+		LastName:  "Pepitos",
+	}, nil
+}
